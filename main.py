9231d596c87970612c63f75bae06ec5132409479
maciekzientarski1234-jpg <maciekzientarski1234@gmail.com>
2026-02-21 14:58:18 +0100

    klikniecie w element

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium.webdriver import Chrome
 from time import sleep
+from selenium.webdriver.common.by import By
 
 # Stworzenie instancji klasy Chrome
 # (to otworzy przeglądarkę)
@@ -9,5 +10,13 @@ driver.get("https://automationpractice.techwithjatin.com/")
 
 # Maksymalizacja okna
 driver.maximize_window()
+
+# Znajdz element Sign in
+sign_in_a = driver.find_element(By.PARTIAL_LINK_TEXT, "Sign in")
+sleep(5)
+# Kliknij w odnaleziony element
+sign_in_a.click()
+
+print(type(sign_in_a))
 sleep(5)
 driver.quit()

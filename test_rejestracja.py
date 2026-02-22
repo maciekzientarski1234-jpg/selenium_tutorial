e06400987e19e26136ae820236af2f5b9ed8e903
maciekzientarski1234-jpg <maciekzientarski1234@gmail.com>
2026-02-22 11:37:10 +0100

    update code

--- test_rejestracja.py
+++ test_rejestracja.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from time import sleep
+from datatools import TestData
+from datatools import Gender
 import unittest
-
 
 
 class RegisterNewUserTest(unittest.TestCase):
@@ -20,7 +21,17 @@ class RegisterNewUserTest(unittest.TestCase):
         # 1. Klinkij "Sign in"
         self.driver.find_element(By.PARTIAL_LINK_TEXT, "Sign in").click()
         # Czekam (usunę po napisaniu testu)
-        sleep(2)
+        # 2. Wpisz e-mail
+        self.driver.find_element(By.ID, "email_create").send_keys(TestData.EMAIL)
+        # 3. Kliknij "Create an account"
+        self.driver.find_element(By.ID, "SubmitCreate").click()
+        # 4. Kliknij swoją płeć
+        if TestData.GENDER == Gender.FEMALE:
+            # Kliknij Mrs
+            self.driver.find_element(By.CSS_SELECTOR, 'label[for="id_gender2"]').click()
+        else:
+            # Kliknij Mr
+            self.driver.find_element(By.XPATH, '//label[@for="id_gender1"]').click()
 
     def tearDown(self):
         self.driver.quit()
